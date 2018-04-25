55aad22d0d8b5e2f1757e2fc32f0278571736c67
Daniel Gallenberger <danielgallenberger@online.de>
2018-04-25 10:54:15 -0700

    feeding demo: move to person

--- feeding/src/main.cpp
+++ feeding/src/main.cpp
@@ -100,7 +100,8 @@ void moveArmToTSR(aikido::constraint::dart::TSR& tsr,
   }
 
   auto testable = std::make_shared<aikido::constraint::Satisfied>(armSpace);
-  aikido::trajectory::TrajectoryPtr timedTrajectory = robot.retimePath(armSkeleton, trajectory.get());
+  auto smoothTrajectory = robot.smoothPath(armSkeleton, trajectory.get(), testable);
+  aikido::trajectory::TrajectoryPtr timedTrajectory = robot.retimePath(armSkeleton, smoothTrajectory.get());
 
   auto future = robot.executeTrajectory(timedTrajectory);
   future.wait();
@@ -191,7 +192,7 @@ int main(int argc, char** argv)
   abovePlateBw(5, 1) = M_PI;
   abovePlateTSR.mBw = abovePlateBw;
 
-  auto marker = viewer.addTSRMarker(abovePlateTSR, 20);
+  //auto marker = viewer.addTSRMarker(abovePlateTSR, 20);
 
   abovePlateTSR.mTw_e.matrix() *= hand->getEndEffectorTransform("plate")->matrix();
   moveArmToTSR(abovePlateTSR, robot, armSpace, armSkeleton, hand);
@@ -223,20 +224,48 @@ int main(int argc, char** argv)
   foodTSR.mBw = nearFoodBw;
   foodTSR.mTw_e.matrix() *= hand->getEndEffectorTransform("plate")->matrix();
 
-  // ***** MOVE ABOVE FOOD *****
+  // ***** MOVE ABOVE FOOD, INTO FOOD AND ABOVE PLATE *****
 
   aikido::constraint::dart::TSR aboveFoodTSR(foodTSR);
   aboveFoodTSR.mTw_e.translation() = Eigen::Vector3d{0, 0, heightAboveFood};
 
-  auto marker2 = viewer.addTSRMarker(aboveFoodTSR, 20);
+  //auto marker2 = viewer.addTSRMarker(aboveFoodTSR, 20);
   moveArmToTSR(aboveFoodTSR, robot, armSpace, armSkeleton, hand);
 
   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-  auto marker3 = viewer.addTSRMarker(foodTSR, 20);
+  //auto marker3 = viewer.addTSRMarker(foodTSR, 20);
   moveArmToTSR(foodTSR, robot, armSpace, armSkeleton, hand);
 
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   moveArmToTSR(abovePlateTSR, robot, armSpace, armSkeleton, hand);
+
+
+  // ***** MOVE TO PERSON *****
+
+  auto personTSR = pr_tsr::getDefaultCanTSR();
+  Eigen::Isometry3d personPose;
+  personPose = Eigen::Isometry3d::Identity();
+  personPose.translation() = Eigen::Vector3d(0.2, -0.32525,  0.602);
+  personTSR.mT0_w = personPose;
+  personTSR.mTw_e.translation() = Eigen::Vector3d{0, 0, 0};
+
+
+  double horizontal_tolerance_near_person = 0.1;
+  double vertical_tolerance_near_person = 0.1;
+
+  Eigen::MatrixXd personBw = Eigen::Matrix<double, 6, 2>::Zero();
+  personBw(0, 0) = -horizontal_tolerance_near_person;
+  personBw(0, 1) = horizontal_tolerance_near_person;
+  personBw(1, 0) = -horizontal_tolerance_near_person;
+  personBw(1, 1) = horizontal_tolerance_near_person;
+  personBw(2, 0) = -vertical_tolerance_near_person;
+  personBw(2, 1) = vertical_tolerance_near_person;
+  personTSR.mBw = personBw;
+  personTSR.mTw_e.matrix() *= hand->getEndEffectorTransform("person")->matrix();
+
+  //auto marker4 = viewer.addTSRMarker(personTSR, 20);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+  moveArmToTSR(personTSR, robot, armSpace, armSkeleton, hand);
 
   std::cin.get();
   return 0;
