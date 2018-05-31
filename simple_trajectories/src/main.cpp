6748d290ab4f60c0665513b5c5f149202a6a78fe
Daqing Yi <dqyi@ava.personalrobotics.ri.cmu.edu>
2018-05-31 10:31:08 -0700

    new ada desc

--- simple_trajectories/src/main.cpp
+++ simple_trajectories/src/main.cpp
@@ -188,8 +188,8 @@ int main(int argc, char** argv)
   if (adaSim)
   {
     Eigen::VectorXd home(Eigen::VectorXd::Zero(6));
-    home[1] = -3.14;
-    home[2] = -3.14;
+    home[1] = 3.14;
+    home[2] = 3.14;
     armSkeleton->setPositions(home);
 
     auto startState
@@ -246,17 +246,17 @@ int main(int argc, char** argv)
   std::cout << "END VALIDATION " << std::endl;
 
   Eigen::VectorXd startConfig(6);
-  startConfig << 0.1, -3.0, -3.0, 0.0, 0.0, 0.0;
+  startConfig << 0.1, 3.0, 3.0, 0.0, 0.0, 0.0;
   moveArmTo(robot, armSpace, armSkeleton, startConfig);
 
   waitForUser("Press key to continue.");
   Eigen::VectorXd viaConfig(6);
-  viaConfig << 0.1, -2.9, -2.9, 0.1, 0.0, 0.0;
+  viaConfig << 0.1, 2.9, 2.9, 0.1, 0.0, 0.0;
   Eigen::VectorXd viaVelocity(6);
-  viaVelocity << 0.0, 0.8, 0.8, 0.8, 0.0, 0.0;
+  viaVelocity << 0.0, -0.8, -0.8, 0.8, 0.0, 0.0;
 
   Eigen::VectorXd goalConfig(6);
-  goalConfig << 0.1, -1.0, -1.0, 0.6, 0.0, 0.0;
+  goalConfig << 0.1, 1.0, 1.0, 0.6, 0.0, 0.0;
  
   ROS_INFO("Starting the kinodynamic testing");
   moveArmTo(robot, armSpace, armSkeleton, 
