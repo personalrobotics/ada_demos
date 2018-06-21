1080ef3901c6ae871b6650a50142638f715e4b0a
Daniel Gallenberger <danielga@cs.uw.edu>
2018-06-21 14:32:11 -0700

    reenabled FTThreshold

--- feeding/src/main.cpp
+++ feeding/src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
   // start demo
   FeedingDemo feedingDemo(adaReal, nodeHandle);
 
-  FTThresholdHelper ftThresholdHelper(false, nodeHandle);
+  FTThresholdHelper ftThresholdHelper(adaReal, nodeHandle);
 
   Perception perception(
       feedingDemo.getWorld(), *feedingDemo.getAda(), nodeHandle);
