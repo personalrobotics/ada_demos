dbee81343f21020fca7b3f87eb216cc2952890be
Daniel Gallenberger <danielga@cs.uw.edu>
2018-10-09 14:10:07 -0700

    adapted force and main function

--- feeding/src/main.cpp
+++ feeding/src/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char** argv)
 
   feedingDemo.moveToStartConfiguration();
 
-  return feeding::studydmain(feedingDemo,
+  return feeding::acquisitiontiltedmain(feedingDemo,
                      ftThresholdHelper,
                      perception,
                      viewer,
