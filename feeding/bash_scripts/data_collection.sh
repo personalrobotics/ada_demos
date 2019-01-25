bb8e1bc32d7a9c014d7ff7ab5ffaccadfda249e1
Gilwoo Lee <gilwoo301@gmail.com>
2019-01-25 15:39:30 -0800

    add data collection bash script

---
+++ feeding/bash_scripts/data_collection.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+WSPATH=$(catkin locate)
+
+cameraInfo="/camera/aligned_depth_to_color/camera_info"
+cameraAlignedDepthImageRaw="/camera/aligned_depth_to_color/image_raw"
+colorCameraInfo="/camera/color/camera_info"
+colorCameraRaw="/camera/color/image_raw"
+cameraExtrinsics="/camera/extrinsics/depth_to_color"
+jointStates="/joint_states"
+forque="/forque/forqueSensor"
+tf="tf"
+tfStatic="/tf_static"
+
+name=$1"-"$2"-"$3
+source ${WSPATH}/devel/setup.bash
+rosbag record $cameraInfo $cameraAlignedDepthImageRaw $colorCameraInfo $colorCameraRaw $cameraExtrinsics $jointStates $forque $tf $tfStatic -O /home/herb/feeding/rosbag/$name /topic __name:=my_bag &
+
+/home/herb/Workspace/gilwoo_ws/devel/bin/feeding -d collect_skewer --foodName $1 --direction $2 --trial $3 -af
+
+echo "Kill rosbag"
+rosnode kill /my_bag
+
+sleep 1
+pkill -f rosbag
