681af0fe19197fdc0824a479f251560dc0870cac
Ethan Gordon <ekgordon@cs.washington.edu>
2019-08-21 13:16:24 -0700

    Updated to match incoming AIKIDO change

--- feeding/src/perception/PerceptionServoClient.cpp
+++ feeding/src/perception/PerceptionServoClient.cpp
@@ -400,7 +400,7 @@ PerceptionServoClient::createPartialTimedTrajectoryFromCurrentConfig(
       mMetaSkeleton->getPositions(), state);
 
   double refTime = findTimeOfClosestStateOnTrajectory(
-      *trajectory, mMetaSkeleton, state, distance, 0.01);
+      *trajectory, state, distance, 0.01);
 
   if (distance > 1.0)
   {
