2692db35a404e77a4e5e9e57120d7c2f7ca77950
Matt Barnes <barnes.matt.j@gmail.com>
2019-11-06 12:25:44 -0800

    Still debugging the inference forward pass.

--- feeding/python/acquisition_detector.py
+++ feeding/python/acquisition_detector.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from ada_demos.srv import DetectAcquisition, DetectAcquisitionResponse
-from google_drive_downloader import GoogleDriveDownloader as gdd
+from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 from os import path
 import rospy
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
 import torch
 from torchvision import models, transforms
 
 
-MODEL_PATH = 'detector_ckpt.pth'
+MODEL_PATH = '../bash_scripts/checkpoint/squeezenet.pth'
 
 
 def detector():
@@ -30,6 +30,7 @@ class InferenceModel(object):
         # Initialize and load model
         self.model = models.squeezenet1_0(pretrained=False)
         self.model.classifier[1] = torch.nn.Conv2d(512, 2, kernel_size=(1, 1), stride=(1, 1))
+        self.model.num_classes = 2
         self.model.load_state_dict(torch.load(MODEL_PATH))
 
         # Transforms
@@ -40,15 +41,26 @@ class InferenceModel(object):
         #self.model = model.to(self.device)
         self.model.eval()
 
+        self.bridge = CvBridge()
+
     def handle_detection(self, req):
-        msg = rospy.wait_for_message('/camera/color/image_raw/compressed', CompressedImage)
-        #msg = rospy.wait_for_message('/camera/color/image_raw/', msg_type)
+        #msg = rospy.wait_for_message('/camera/color/image_raw/compressed', CompressedImage)
+        msg = rospy.wait_for_message('/camera/color/image_raw/', Image)
 
         # Tensor transform
-        np_arr = np.fromstring(msg.data, np.uint8)
-        img = torch.tensor(np_arr)
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as s:
+            print(s)
+        img = np.asarray(cv_image).astype(np.float32)
+        img = img.transpose((2, 0, 1))  # HWC --> CHW
+        img = torch.tensor(img)
+
+        print(img.size())
         img = self.color_normalizer(img)
 
+        img.unsqueeze_(0)  # batch size 1
+        print(img.size())
         #inputs = sample['image'].to(device)
         with torch.set_grad_enabled(False):
             outputs = self.model(img)
